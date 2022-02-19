8f6bee1e7f92d2cc7ab45b01e108b67459b02f99
Erick Arostegui Cunza <scorpius86@gmail.com>
2022-02-19 02:31:20 +0100

    Session 04

---
+++ 04/Net5.Fundamentals/Net5.Fundamentals.ListAndThread/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+
+namespace Net5.Fundamentals.ListAndThread
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            string opt;
+            bool exit = false;
+            ListAndThread listAndThread = new ListAndThread();
+
+            do
+            {
+                Console.Clear();
+                Console.WriteLine("List And Thread");
+                Console.WriteLine("===============");
+                Console.WriteLine(" 1 - Array");
+                Console.WriteLine(" 2 - List");
+                Console.WriteLine(" 3 - Dictionary");
+                Console.WriteLine(" 4 - SorteList");
+                Console.WriteLine(" 5 - HashTable");
+                Console.WriteLine(" 6 - Stack");
+                Console.WriteLine(" 7 - Queue");
+                Console.WriteLine(" 8 - Tuple");
+                Console.WriteLine(" 9 - ValueTuple");
+                Console.WriteLine("10 - Thread");
+                Console.WriteLine("11 - Task");
+                Console.WriteLine("12 - StreamReader");
+                Console.WriteLine("13 - StreamWriter");
+                Console.WriteLine("99 - exit");
+                Console.WriteLine("");
+                Console.Write("Seleccione una opcion : ");
+                opt = Console.ReadLine();
+                Console.Clear();
+
+                switch (opt)
+                {
+                    default:
+                        break;
+                }
+
+                if (!exit)
+                {
+                    Console.WriteLine("");
+                    Console.WriteLine("Presione un a tecla para continuar");
+                    Console.ReadKey();
+                }
+
+            } while (!exit);
+        }
+    }
+}
