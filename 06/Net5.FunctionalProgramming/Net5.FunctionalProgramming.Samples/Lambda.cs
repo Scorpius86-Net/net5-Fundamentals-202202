13459e684ba38dde1b5bdd8df4fc9bec61830512
Erick Arostegui Cunza <scorpius86@gmail.com>
2022-02-26 03:54:34 +0100

    Session 06

---
+++ 06/Net5.FunctionalProgramming/Net5.FunctionalProgramming.Samples/Lambda.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Net5.FunctionalProgramming.Samples
+{
+    public class Lambda
+    {
+        delegate int operationDelegate(int x, int y);
+        public void DoWork()
+        {
+            Print(5, 5, Suma);
+            Print(5, 5, (int a, int b)=>
+            {
+                //To Do
+                //To Do
+                //To Do
+                //To Do
+                return a + b;
+            });
+            Print(5, 5, (a, b) => a + b);
+            Print(4, 2, (x, y) => x * y);
+            Print(6, 6, (a, b) => a + b);
+            Print(144, 666, (x, y) => x * y);
+
+            FunctionDelegate();
+            ActionDelegate();
+        }
+
+        private void FunctionDelegate()
+        {
+
+            Func<string, string> selector = ToUpper;
+            List<string> words = new List<string> { "Naranja", "Manzana", "Articulo", "elefante" };
+            //List<string> aWords = words.Select(ToUpper).ToList();
+            //IEnumerable<string> aWords = words.Select((str) =>
+            //{
+            //    return str.ToUpper();
+            //});
+            //IEnumerable<string> aWords = words.Select((str) => str.ToUpper());
+            List<string> aWords = words.Select(selector).ToList();
+
+            aWords.ForEach(word =>
+            {
+                Console.WriteLine(word);
+            });
+        }
+
+        private void ActionDelegate()
+        {
+            Action<string> messageTarget;
+            string[] args = Environment.GetCommandLineArgs();
+
+            for (int i = 0; i < args.Length; i++)
+            {
+                Console.WriteLine($"args[{i}] = {args[i]}");
+            }
+            
+            if (args.Length > 1)
+            {
+                messageTarget = ShowWindowsMessage;
+            }
+            else
+            {
+                messageTarget = Console.WriteLine;
+            }
+
+            if (args.Length > 1)
+            {
+                messageTarget(args[1]);
+            }
+            else
+            {
+                messageTarget("No tiene argumentos");
+            }
+        }
+
+        private int Suma(int a,int b)
+        {
+            return a + b;
+        }
+        private void Print(int a,int b,operationDelegate operation)
+        {
+            int result = operation(a,b);
+            Console.WriteLine($"Imprimiendo : {result}");
+        }
+
+        private string ToUpper(string s)
+        {
+            return s.ToUpper();
+        }
+
+        private void ShowWindowsMessage(string message)
+        {
+            Console.WriteLine(message);
+        }
+    }
+}
---
+++ 06/Net5.FunctionalProgramming/Net5.FunctionalProgramming.Samples/Program.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Net5.FunctionalProgramming.Samples
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Alumno> alumnos = new List<Alumno>();
+            LoadAlumnos(alumnos);
+
+            bool isErick = false;
+            
+            foreach (Alumno alumno in alumnos)
+            {
+                /*
+                if (alumno.FirstName.Equals("Erick"))
+                {
+                    isErick = true;
+                    break;
+                }
+                */
+                if (IsErick(alumno.FirstName))
+                {
+                    isErick = true;
+                    break;
+                }
+
+            }
+
+            if (isErick)
+            {
+                Console.WriteLine("Found Erick");
+            }
+            else
+            {
+                Console.WriteLine("Not Found Erick");
+            }
+
+            /////////////////////////////////////////////           
+
+            if (alumnos.Any(alumno => IsErick(alumno.FirstName)))
+            {
+                Console.WriteLine("Found Erick");
+            }
+            else
+            {
+                Console.WriteLine("Not Found Erick");
+            }
+
+            /////////////////////////////////////////////           
+
+            if (alumnos.Any(alumno => alumno.FirstName.Equals("Erick")))
+            {
+                Console.WriteLine("Found Erick");
+            }
+            else
+            {
+                Console.WriteLine("Not Found Erick");
+            }
+
+            Lambda lambda = new Lambda();
+            lambda.DoWork();
+
+        }
+
+        static bool IsErick(string firstName)
+        {
+            if (firstName.Equals("Erick"))
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        static void LoadAlumnos(List<Alumno> alumnos)
+        {
+            for (int i = 0; i < 10; i++)
+            {
+                Alumno alumno = new Alumno { FirstName = $"Alumno-{i + 1}" };
+                alumnos.Add(alumno);
+            }
+
+            alumnos.Add(new Alumno { FirstName = "Erick" });
+        }
+
+    }
+}
