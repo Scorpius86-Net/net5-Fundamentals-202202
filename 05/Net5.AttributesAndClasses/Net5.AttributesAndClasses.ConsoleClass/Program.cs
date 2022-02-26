0c0774ee0705f4d46858f9c2cec2448ac85e6966
Erick Arostegui Cunza <scorpius86@gmail.com>
2022-02-26 01:52:06 +0100

    Session 05

--- 05/Net5.AttributesAndClasses/Net5.AttributesAndClasses.ConsoleClass/Program.cs
+++ 05/Net5.AttributesAndClasses/Net5.AttributesAndClasses.ConsoleClass/Program.cs
@@ -1,5 +1,8 @@
-﻿using Net5.AttributesAndClasses.LibraryClasses;
+﻿using Net5.AttributesAndClasses.ConsoleClass.Entities;
+using Net5.AttributesAndClasses.ConsoleClass.Repositories;
+using Net5.AttributesAndClasses.LibraryClasses;
 using System;
+using System.Collections.Generic;
 
 namespace Net5.AttributesAndClasses.ConsoleClass
 {
@@ -52,6 +55,36 @@ namespace Net5.AttributesAndClasses.ConsoleClass
 
             Cat cat = new Cat();
             cat.Run();
+
+            //ProductRepository productRepository = new ProductRepository();            
+            //productRepository.Insert(new Product { ProductId = 1, Descrption = "Teclado" });
+
+            GenericRepository<Product> productRepository = new GenericRepository<Product>();
+            productRepository.Insert(new Product { ProductId = 1, Descrption = "Teclado" });
+
+            Product mouse = new Product { ProductId = 2, Descrption = "Mouse" };
+            productRepository.Insert(mouse);
+
+            Product monitor = new Product { ProductId = 3, Descrption = "Monitor" };
+            productRepository.Insert(monitor);
+
+            Product monitor2 = new Product { ProductId = 3, Descrption = "Monitor V2" };
+            productRepository.Update(monitor,monitor2);
+
+            productRepository.Delete(mouse);
+
+            List<Product> products = productRepository.List();
+
+            GenericRepository<Customer> customerRepository = new GenericRepository<Customer>();
+            Customer customer = new Customer { CustomerId = 1, Name = "Erick", LastName = "Aróstegui" };
+            customerRepository.Insert(customer);
+            List<Customer> customers = customerRepository.List();
+
+            GenericRepository<Sale> saleRepository = new GenericRepository<Sale>();
+            Sale sale = new Sale { Id = 1, Description = "Venta de Laptop", Total = 200 };
+            saleRepository.Insert(sale);
+            List<Sale> sales = saleRepository.List();
+
         }
 
         static public void RunDelegate(MyDelegate myDelegate, int i)
