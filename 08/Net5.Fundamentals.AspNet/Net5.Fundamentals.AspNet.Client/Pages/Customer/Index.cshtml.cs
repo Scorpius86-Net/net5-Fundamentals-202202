5dcaba429bf785d02a0764d4848cd0c429aa971b
Daniel Alexis Moreno Herrera <daniel10697@hotmail.com>
2022-03-05 01:53:42 +0100

    Session 08

---
+++ 08/Net5.Fundamentals.AspNet/Net5.Fundamentals.AspNet.Client/Pages/Customer/Index.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Net5.Fundamentals.AspNet.Client.Pages.Customer
+{
+    public class IndexModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
