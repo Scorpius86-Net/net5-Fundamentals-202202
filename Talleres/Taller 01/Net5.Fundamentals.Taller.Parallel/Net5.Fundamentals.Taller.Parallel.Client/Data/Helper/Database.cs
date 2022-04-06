d989908995a678f0b0b717c8401cb7750bf8b69f
Erick Aróstegui Cunza <Scorpius86@gmail.com>
2022-04-06 03:39:42 +0200

    Add Normal Query

---
+++ Talleres/Taller 01/Net5.Fundamentals.Taller.Parallel/Net5.Fundamentals.Taller.Parallel.Client/Data/Helper/Database.cs
@@ -0,0 +1,61 @@
+﻿using Net5.Fundamentals.Taller.Parallel.Client.Data.Entities;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Net5.Fundamentals.Taller.Parallel.Client.Data.Helper
+{
+    public class Database
+    {
+        public List<Badge> ListBadges()
+        {
+            Console.WriteLine("ListBadges Start");
+
+            List<Badge> badges = new List<Badge>();
+
+            string strConn = "Data Source=.;Initial Catalog=StackOverflow2013;User ID=sa;Password=Password1234";
+            using (SqlConnection conn = new SqlConnection(strConn))
+            {
+                string query = @"
+                    SELECT 
+                        [Id],
+                        [Name],
+                        [UserId],
+                        [Date]
+                    FROM [StackOverflow2013].[dbo].[Badges]
+                    ORDER BY [Id]
+                ";
+
+                using (SqlCommand cmd = new SqlCommand(query, conn))
+                {
+                    conn.Open();
+                    SqlDataReader reader =  cmd.ExecuteReader();
+
+                    if (reader.HasRows)
+                    {
+                        while (reader.Read())
+                        {
+                            Badge badge = new Badge();
+                            badge.Id = reader.GetInt32(0);
+                            badge.Name = reader.GetString(1);
+                            badge.UserId = reader.GetInt32(2);
+                            badge.Date = reader.GetDateTime(3);
+
+                            badges.Add(badge);
+                        }
+                    }
+
+                    reader.Close();
+                    conn.Close();
+                }
+
+                Console.WriteLine("ListBadges End");
+
+                return badges;
+            }
+        }
+    }
+}
---
+++ Talleres/Taller 01/Net5.Fundamentals.Taller.Parallel/Net5.Fundamentals.Taller.Parallel.Client/Program.cs
@@ -0,0 +1,52 @@
+﻿using Net5.Fundamentals.Taller.Parallel.Client.Data.Entities;
+using Net5.Fundamentals.Taller.Parallel.Client.Data.Helper;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Net5.Fundamentals.Taller.Parallel.Client
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            bool exit = false;
+            Database database = new Database();
+            List<Badge> badges = new List<Badge>();
+            string opt;
+
+            do
+            {
+                Console.Clear();
+                Console.WriteLine("Taller - Parallel");
+                Console.WriteLine("=================");
+                Console.WriteLine(" 1 - Normal Query");
+                Console.WriteLine(" 2 - Parallel Query");
+                Console.WriteLine("99 - Exit");
+                opt = Console.ReadLine();
+
+                switch (opt)
+                {
+                    case "1":
+                        badges = database.ListBadges();
+                        Console.WriteLine($"ListBadges, Count : {badges.Count()}");
+                        break;
+                    case "2":
+                        break;
+                    case "99":
+                        exit = true;
+                        break;
+                    default:
+                        break;
+                }
+
+                if (!exit)
+                {
+                    Console.WriteLine();
+                    Console.WriteLine("Press any key to continue");
+                    Console.ReadKey();
+                }
+            }while (!exit);
+        }
+    }
+}
